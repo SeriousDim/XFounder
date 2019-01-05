10f4701c13a80f3c5517a83356479ed2aeb2852d
rivero_off <nichirivero@gmail.com>
2019-01-05 11:23:52 +0300

    the first one

---
+++ app/src/main/java/com/xproject/eightstudio/x_project/MainActivity.java
@@ -0,0 +1,50 @@
+package com.xproject.eightstudio.x_project;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    mTextMessage.setText(R.string.title_home);
+                    return true;
+                case R.id.navigation_employees:
+                    mTextMessage.setText(R.string.title_employees);
+                    return true;
+                case R.id.navigation_chat:
+                    mTextMessage.setText(R.string.title_chat);
+                    return true;
+                case R.id.navigation_projects:
+                    mTextMessage.setText(R.string.title_projects);
+                    return true;
+                case R.id.navigation_companies:
+                    mTextMessage.setText(R.string.title_companies);
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+}
